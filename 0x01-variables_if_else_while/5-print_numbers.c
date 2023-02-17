ab1fb2181a58cd329608696b2cd78cf1beec472a
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-17 15:14:25 -0800

    5-print_numbers.c added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", n);
+	putcar('\n');
+	return (0);
+}
