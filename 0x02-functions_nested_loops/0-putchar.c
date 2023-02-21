d193435199e9bfcf0426ea8eeec1755401a3e7a6
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-21 11:22:08 -0800

    0-putchar.c updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - entry point
  *
@@ -16,6 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar'\n');
+	_putchar('\n');
 	return (0);
 }
