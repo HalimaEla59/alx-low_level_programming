dde186637b51c6c8f762439b4847882d486518dc
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-21 11:57:29 -0800

    1-alphabet.c added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,33 @@
  *
  */
 int _putchar(char c);
+/**
+ * print_alphabet - prints alphabets
+ *
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void);
+/**
+ * _islower - checks for lowercase character
+ *
+ */
+int _islower(int c);
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ */
+int _isalpha(int c);
+/**
+ * print_sign - prints the sign of the given number
+ *
+ */
+int print_sign(int n);
+/**
+ * _abs - computes the absolute value
+ *
+ */
+int _abs(int);
