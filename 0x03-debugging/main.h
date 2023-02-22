0cb86bd4c099f72aa107c4a8d8f21ba9428c07fa
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-22 14:07:30 -0800

    2-largest_number.c added and main.h updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks the given number's sign
+ * @i: the given integer
+ */
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of the 3 numbers
+ * @a: first int
+ * @b: second int
+ * @c: third int
+ * Return: largest int
+ */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
