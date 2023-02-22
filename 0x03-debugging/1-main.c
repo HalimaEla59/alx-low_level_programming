24b3ef9b848cd7c5e0b523bddeb2ac02cf78f2b0
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-22 13:23:21 -0800

    1-main.c fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -15,7 +13,8 @@ int main(void)
 
 	while(i < 10)
 	{
-		putchar(i); // the integere i doesn't increment it will always stay 0 which is < 10
+		putchar(i);
+		// the integere i doesn't increment it will always stay 0 which is < 10
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
