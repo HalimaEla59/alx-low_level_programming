8f4b967343af89879c8ed9cf47ca5914c79b1445
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-27 09:45:00 -0800

    1-swap.c added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two int
+ *
+ * @a: first pointer int
+ * @b: second pointer int
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = a;
+	a = b;
+	b = c;
+}
