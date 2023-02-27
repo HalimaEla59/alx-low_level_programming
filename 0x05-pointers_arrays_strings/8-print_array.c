1f50e5dd7197516781d096e12108461ac4e67ee0
HalimaEla59 <halimaelabbadi@gmail.com>
2023-02-27 14:13:46 -0800

    8-print_array.c added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_array - prints n numbers from the the array
+ *
+ * @a: poinet int
+ * @n: number of ints to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n > _strlen(a))
+	{
+		printf("%d is bigger than the array's length\n", n);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a[i]);
+			if (i != n - 1)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
