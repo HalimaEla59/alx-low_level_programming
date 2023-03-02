05255d8dfb8c805b95499e91317b3ea98e5edcfb
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-02 08:37:25 -0800

    6-cap_string.c fixeee

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _indexOf - checks for special characters (boolean)
- * 
- * @c: the given character
- * Return: true or false
- */
-
-int _indexOf(char c)
-{
-	int i;
-	char SCArr[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-
-	for (i = 0; i < 13; i++)
-	{
-		if (SCArr[i] == a)
-			return (1);
-	}
-	return (0);
-}
-
-/**
  * cap_string - capitalizes all the words of a string
  *
  * @s: string
@@ -30,13 +10,16 @@ int _indexOf(char c)
 char *cap_string(char *s)
 {
 	int i;
+	int j;
+	char sp[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (_indexOf(s[i]))
-			continue;
-		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
-			s[i] = s[i] - 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] <= 'a' && s[i] >= 'z' && (s[i - 1] == sp[j] || i == 0))
+				s[i] = s[i] - 32;
+		}
 	}
 	return (s);
 }
