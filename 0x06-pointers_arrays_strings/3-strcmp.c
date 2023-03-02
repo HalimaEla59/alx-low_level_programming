1accd53b626f800c0773ac19fcf6bafe217720d0
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-02 06:39:44 -0800

    3-strcmp.c fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp -- compares two strings
+ * _strcmp - compares two strings
  *
  * @s1: first string
  * @s2: second string
@@ -11,6 +11,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	i = 0;
 	
 	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
