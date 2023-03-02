402ae44b8a87c7131ef370c5335dc6c271f2fa03
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-02 08:12:25 -0800

    6-cap_string.c fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _indexOf - checks for special characters (boolean)
+ * 
+ * @c: the given character
+ * Return: true or false
+ */
+
+int _indexOf(char c)
+{
+	int i;
+	char SpChArray[13] = {' ', '.', ',', ';', '"', '!', '?', '(', ')', '{', '}', '\n', '\t'};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (SpChArray[i] == a)
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * cap_string - capitalizes all the words of a string
  *
  * @s: string
@@ -13,9 +33,9 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (indexOf(s[i]))
+		if (_indexOf(s[i]))
 			continue;
-		if (s[i] >= 'a' && s[i] <= 'z' && (indexOf(s[i - 1]) || i == 0))
+		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
 			s[i] = s[i] - 32;
 	}
 	return (s);
