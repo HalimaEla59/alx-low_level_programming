03a103417792dc4e10a93630a07e6263bdf928b4
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-02 05:36:56 -0800

    0-strcat.c added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,10 +15,14 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
 
 	for (j = 0; src[j] != '\0'; j++)
+	{
 		dest[i + j] = src[j];
+	}
 
 	dest[i + j] = '\0';
 	return (dest);
