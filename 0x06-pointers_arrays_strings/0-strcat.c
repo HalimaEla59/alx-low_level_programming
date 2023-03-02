e1a5e5b6540386d2c31722e364a8790e18d85062
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-02 05:42:52 -0800

    0-strcat.c fixe

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,8 +13,11 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{}
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
@@ -23,3 +26,4 @@ char *_strcat(char *dest, char *src)
 
 	dest[i + j] = '\0';
 	return (dest);
+}
