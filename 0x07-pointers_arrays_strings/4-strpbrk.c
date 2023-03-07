3d430720568b49f7b3b1e671e7b6a824364a2339
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-06 16:57:39 -0800

    4-strpbrk.c fived

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
+	unsigned int i;
+	unsigned int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				return (s[i]);
+			}
 		}
 	}
-	return (0);
+	return (NULL);
 }
