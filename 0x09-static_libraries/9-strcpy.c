c138cf39af7d5846b4e384a8664505ed7d059d35
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-10 06:33:11 -0800

    strlen puts strcpy copied

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copies the string pointed by src to the buffer pointed by dest
+ *
+ * @dest: pointer char
+ * @src: pointer char
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
