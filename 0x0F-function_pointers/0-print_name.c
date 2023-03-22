863dc238c9b0752c122fab3101ad75a04f00b600
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-22 15:28:45 -0700

    0-print_name.c betty fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: input
+ * @f: function to use
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
