eca92d95a0e301f5f61f69972a2e01b6081ec6f5
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-22 17:14:14 -0700

    all 3-* files added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - main fct
+ * @argc: arguments
+ * @argv: array of arg
+ * Return: depends
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		int a;
+		int b;
+		int (*fct)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		fct = get_op_func(argv[2]);
+		if ((*argv[2] == "/" || *argv[2] == "%") && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", fct(a, b));
+		return (0);
+	}
+	printf("Error\n");
+	exit(98);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - returns a+b
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: a+b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns a-b
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: a-b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns a*b
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: a*b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns a/b
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns a%b
+ * @a: 1st int
+ * @b: 2nd int
+ * Returns: a%b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
