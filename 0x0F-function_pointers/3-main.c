7af4a125f952f6f0d1aaffdde13af361e53e2729
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-22 18:08:54 -0700

    3-main.c big fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,19 +15,23 @@ int main(int argc, char *argv[])
 	{
 		int a;
 		int b;
-		int (*func)(int, int);
+		int result;
+		int (*fct)(int, int);
+		char *operation
 
 		a = atoi(argv[1]);
 		b = atoi(argv[3]);
-		func = get_op_func(argv[2]);
+		operation = argv[2];
 
-		if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+		if ((*operation == '/' || *operation == '%') && (b == 0))
 		{
 			printf("Error\n");
 			exit(100);
 		}
 
-		printf("%d\n", func(a, b));
+		fct = get_op_func(operation);
+		result = fct(a, b);
+		printf("%d\n", result);
 		return (0);
 	}
 	
