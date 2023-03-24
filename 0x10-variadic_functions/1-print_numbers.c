f2cf8fb71cb779a44445afcc397ef679ddc8eae2
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-23 17:11:54 -0700

    1-print_numbers.c added

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string between numbers
+ * @n: number of ints passed to fct
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(vl, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(vl, const unsigned int));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(vl);
+}
