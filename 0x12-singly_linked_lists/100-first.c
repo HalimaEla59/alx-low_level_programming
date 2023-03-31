0d656cd399ba9b626b37311bf22f38d425224040
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-30 18:50:00 -0700

    100-first.c fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,9 @@ void print_before_main(void) __attribute__ ((constructor));
 
 void print_before_main(void)
 {
-	char *str;
-	
-	str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", str);
+	char *str1, *str2;
+
+	str1 = "You're beat! and yet, you must allow,\n";
+	str2 = "I bore my house upon my back!\n";
+	printf("%s%s", str1, str2);
 }
