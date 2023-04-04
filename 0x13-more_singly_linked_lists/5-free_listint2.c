ea52e80c65aa78586879276c05fad1d3b2112a31
HalimaEla59 <halimaelabbadi@gmail.com>
2023-04-04 07:21:19 -0700

    some 5-...c fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,8 +13,8 @@ void free_listint2(listint_t **head)
 	{
 		while (head != NULL)
 		{
-			temp = head;
-			head = head->next;
+			temp = *head;
+			*head = temp->next;
 			free(temp);
 		}
 		*head = NULL;
