a8d374a9ad6087daba69c516dd666b0cf788705a
HalimaEla59 <halimaelabbadi@gmail.com>
2023-04-10 20:01:11 -0700

    betty fixe

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_to_text_file - appends txt at the end of file
+ * append_text_to_file - appends txt at the end of file
  * @filename: file name
  * @text_content: NULL terminated string
  * Return: 1 or -1
