43fff983a0e5eac3199d77598fa1a8f302c74d62
HalimaEla59 <halimaelabbadi@gmail.com>
2023-05-08 15:20:33 -0700

    1-... fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "main.h"*
+#include "main.h"
 
 /**
  * create_file - craetes a new file
@@ -16,8 +16,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_CREAT | o_TRUNC | O_RDWR, 0600);
-	if (fp = -1)
+	fp = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fp == -1)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
