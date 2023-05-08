3f9ed691231deaf658d860b5b276dae956dc42d3
HalimaEla59 <halimaelabbadi@gmail.com>
2023-05-08 15:26:56 -0700

    2-... continued

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,3 +7,29 @@
  * Return: 1 or -1
  */
 
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len = 0;
+	int fd;
+	int Write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	Write = write(fd, text_content, len);
+	if (Write == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
