6b729d587ef581a4a5e62db9c0e33b9b84cba840
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-07-07 01:27:23 +0100

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: string of 0s and 1s
+ * Return: converted number or 0 if error
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		else if (b[i] == '0')
+			result <<= 1;
+		else if (b[i] == '1')
+			result = (result << 1) | 1;
+	}
+
+	return (result);
+}
