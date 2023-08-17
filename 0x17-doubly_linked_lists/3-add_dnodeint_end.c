58c2c55e00bbde36a6e048313a10f4313199419f
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-18 00:23:02 +0100

    betty 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
   
 	res->n = n;
-  res->prev = NULL;
+	res->prev = NULL;
 	res->next = NULL;
 	if ((*head) == NULL)
 	{
