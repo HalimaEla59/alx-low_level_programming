ad44fbe244ecfe392e412c777a539a5bae59780d
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-21 23:06:54 +0100

    Create 2-strlen.c

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: pointer char
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
