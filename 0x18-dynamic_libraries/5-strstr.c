1c9b77ee598a141b65529a6b9b4daf31341791fb
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-21 23:16:54 +0100

    Create 5-strstr.c

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - finds the first occurence of the substring needle in the string haystack
+ * @haystack: the string
+ * @needle: the substring
+ * Return: pointes to the begining of the located substring, NULL otherwise
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return (NULL);
+}
