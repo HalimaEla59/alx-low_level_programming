2e8c078131fd68086f93cac6e59bd3705173438d
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-08 00:43:26 +0100

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table
+ * @size: size of table
+ * Return: a pointer to a created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+  unsigned int i;
+	hash_table_t *resTab;
+
+	if (size > 0)
+	{
+		resTab = malloc(sizeof(hash_table_t));
+		if (!resTab)
+			return (NULL);
+    
+		resTab->size = size;
+		resTab->array = malloc(sizeof(hash_node_t *) * size);
+		if (!resTab->array)
+		{
+			free(resTab);
+			return (NULL);
+		}
+		for (i = 0; i < size; i++)
+			resTab->array[i] = NULL;
+		return (resTab);
+	}
+	return (NULL);
+}
