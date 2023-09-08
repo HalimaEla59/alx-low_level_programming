d45efb2d01b2a9385de81d264a5d2da183cfccd4
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-08 01:35:48 +0100

    Create 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *htarr, *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		htarr = ht->array[i];
+		while (htarr)
+		{
+			temp = hatarr->next;
+			free(htarr->key);
+			free(htarr->value);
+			free(htarr);
+			htarr = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
