c64b76f4d581792c4354bbc259f359ae1bba7b7d
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-10-25 23:40:44 +0000

    5-... fixing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,24 @@
 
 
 def island_perimeter(grid):
-  """grid is a list of list of integers: 0 water zone, 1 land"""
+    """grid is a list of list of integers: 0 water zone, 1 land"""
     perimeter = 0
-if grid:
-height = len(grid)
-width = len(grid[0])
-    for i in range(0, height):
-        for j in range(0, width):
-            if grid[i][j] == 1:
-                if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
-                    perimeter += 1
-                if j > 0 and grid[i][j - 1] == 0:
-                    perimeter += 1
-                if i > 0 and grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i < height - 1 and grid[i + 1][j] == 0:
-                    perimeter += 1
-                if i == 0 or i == height - 1:
-                    perimeter += 1
-                if j == 0 or j == len(grid[i]) - 1:
-                    perimeter += 1
+    if grid:
+        height = len(grid)
+        width = len(grid[0])
+        for i in range(0, height):
+            for j in range(0, width):
+                if grid[i][j] == 1:
+                    if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
+                        perimeter += 1
+                    if j > 0 and grid[i][j - 1] == 0:
+                        perimeter += 1
+                    if i > 0 and grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if i < height - 1 and grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if i == 0 or i == height - 1:
+                        perimeter += 1
+                    if j == 0 or j == len(grid[i]) - 1:
+                        perimeter += 1
     return perimeter
